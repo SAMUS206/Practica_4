680dc1dcbb76d1c414fcdb360a72c73e397c7765
SAMUS <usa06565@gmail.com>
2024-09-05 19:40:02 -0600

    name page

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -29,7 +29,7 @@ function UserNavbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">USocial</a>
+                <a className="navbar-brand" href="#">ECYS</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
